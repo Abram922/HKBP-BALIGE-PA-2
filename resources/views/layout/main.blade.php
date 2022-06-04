2d5e72fcb436610f1f74a0c5cc772957f2c10791
Abra <abrampane922@gmail.com>
2022-06-04 20:44:48 +0700

    hi

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -33,13 +33,11 @@
 </head>
 
 <body>
-
-
-
-
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
+        <div class=" container">
+            <a class="navbar-brand">
+                <img src="/images/logohkbp.png" width="160" height="100" alt="" margin-left="100px">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- resources/views/layout/user.blade.php
+++ resources/views/layout/user.blade.php
@@ -39,10 +39,9 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <a class="navbar-brand" href="#">
+                <img src="/images/logohkbp.png" width="30" height="30" alt="">
+            </a>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
