c3e00774ab55fd83703611b141632b27f8b224ae
Abra <abrampane922@gmail.com>
2022-06-05 19:55:44 +0700

    y

--- resources/views/autentikasi/welcomependeta.blade.php
+++ resources/views/autentikasi/welcomependeta.blade.php
@@ -19,7 +19,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Jumlah Berita di-Upload</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $countberita }}</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
