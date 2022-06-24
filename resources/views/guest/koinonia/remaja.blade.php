f98acb176b68c4b89cb7f9a934f40ae0f6094367
Abra <abrampane922@gmail.com>
2022-06-24 23:46:15 +0700

    knaba

--- resources/views/guest/koinonia/remaja.blade.php
+++ resources/views/guest/koinonia/remaja.blade.php
@@ -1,86 +1,43 @@
 @extends('layout.main')
-{{-- <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}"> 
+{{-- <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
 <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/css/ionicons.min.css">
-<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}"> 
+<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('css/zoomgambar.css') }}"> --}}
 
 
-{{-- newest --}}
 @section('container')
-    <br>
-    <div class="container">
-        <h2 style="color:#711A75;"><b>Remaja</b></h2>
-        <hr>
-<<<<<<< Updated upstream
-</div>
-@if($remaja->count())
-
-
-
-
-@foreach($remaja as $remajas)
+<br>
 <div class="container">
-<div class="article" >
-    <div class="no-gutters">
-    <h4 style="color:#711A75;"><b>{{ $remajas-> name}}</b></h4>
-    <small>
-        <span><i>{{ $remajas->created_at}}</i></span>
-    </small><br>
-        <div class="col-md-4">
-            <img src="/image/{{  $remajas->image }}" class="card-img" alt="..." width="500" >
-        </div>
-        <div class="col-md-6">
-          <br>
-                <h6 class="text" style="font-style=poppins;">{!!  $remajas -> detail !!}</h6>
-            
-=======
+    <h2 style="color:#711A75;"><b>Remaja</b></h2>
+    <hr>
 
-        <div class="container">
-            <div class="row mb-5">
-                @foreach ($remaja as $remajas)
-                    <div class="article">
-                        <div class="no-gutters">
-                            {{-- <h4 style="color:#711A75;"><b>{{ $remajas->judul }}</b></h4> --}}
-                            <h4>{{$remajas->name}}</h4>
-                            <div class="col-md-4">
-                                <img src="/image/{{ $remajas->image }}" class="card-img" alt="..." style="height:367px; width:550px">
-                            </div>
-                            <div class="col-md-6">
-                                <br>
-                                
-                                <h6 class="text" style="font-style=poppins;">{!! $remajas->detail !!}</h6>
-
-                            </div>
-                        </div>
+    <div class="container">
+        <div class="row mb-5">
+            @foreach ($remaja as $remajas)
+            <div class="article">
+                <div class="no-gutters">
+                    {{-- <h4 style="color:#711A75;"><b>{{ $remajas->judul }}</b></h4> --}}
+                    <h4>{{$remajas->name}}</h4>
+                    <div class="col-md-4">
+                        <img src="/image/{{ $remajas->image }}" class="card-img" alt="..." style="height:367px; width:550px">
                     </div>
-                    <br><br>
-                    <hr>
-                @endforeach
-                {!! $remaja->links() !!}
+                    <div class="col-md-6">
+                        <br>
+
+                        <h6 class="text" style="font-style=poppins;">{!! $remajas->detail !!}</h6>
+
+                    </div>
+                </div>
             </div>
->>>>>>> Stashed changes
+            <br><br>
+            <hr>
+            @endforeach
+            {!! $remaja->links() !!}
         </div>
-        
     </div>
-    </div>
-<<<<<<< Updated upstream
+
 </div>
 </div>
-</div>
-<br><br>
-<hr>
-
-@endforeach
-@else
-<p>No Post found</p>
-@endif
-
-{{$remaja->links() }}
-
-=======
->>>>>>> Stashed changes
 @endsection
-
-{{-- end newest --}}
