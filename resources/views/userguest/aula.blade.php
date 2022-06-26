b92038f50e8a87227e7ca9ba8da3ee67f0d30c96
Abra <abrampane922@gmail.com>
2022-06-26 14:04:04 +0700

    jdrfgh

--- resources/views/userguest/aula.blade.php
+++ resources/views/userguest/aula.blade.php
@@ -8,15 +8,14 @@
 
 
 
-
+l
 
 
 <div class="container">
     <main>
         <div class="py-5 text-center">
-            <img class="d-block mx-auto mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-            <h2>Checkout form</h2>
-            <p class="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+            <h2>Pesan Aula</h2>
+            <p class="lead">Lengkapi Form Berikut Untuk Melakukan Pemesanan</p>
         </div>
 
         <div class="row g-5">
