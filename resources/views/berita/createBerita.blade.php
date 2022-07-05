c81c3c30f51c0c2b3577585421af573ac31b854c
onainadapdap1 <onainadapdap1@gmail.com>
2022-07-05 10:49:09 +0700

    beritas up

--- resources/views/berita/createBerita.blade.php
+++ resources/views/berita/createBerita.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.admin')
+{{-- @extends('layout.admin')
 
 @section('content')
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.js"></script>
@@ -59,3 +59,81 @@
 @section('ckeditor')
 
 @endsection
+
+
+reungang --}}
+
+<html>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="card mt-5 d-flex justify-content-center">
+            <div class="card-header text-center">
+                <strong>TAMBAH BERITA</strong>
+            </div>
+
+            {{-- card body --}}
+            <div class="card-body">
+
+                <form action="{{ route('beritas.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Title</label>
+                        <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title" autofocus value="{{old('title')}}">
+                        @error('title')
+                        <div class="invalid-feedback">
+                            {{$message}}
+                        </div>
+                        @enderror
+                    </div>
+        
+        
+                    <div class="mb-3">
+                        <label for="image" class="form-label">Image</label>
+                        <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image">
+                        @error('image')
+                        <div class="invalid-feedback">
+                            {{$message}}
+                        </div>
+                        @enderror
+                    </div>
+        
+        
+                    <div class="mb-3">
+                        <label for="body" class="form-label">Body</label>
+                        <textarea class="form-control" id="body" rows="10" name="body"></textarea>
+                    </div>
+                    <div class="form-group mt-2 ">
+                        <button type="submit" class="btn btn-success">Simpan</button>
+                    </div>
+                </form>
+
+                <a href="/beritas">
+                    <button class="btn btn-primary" style="float: right">Kembali</button>
+                </a>
+
+                
+
+                <script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script>
+                <script>
+                    ClassicEditor
+                        .create(document.querySelector('#body'))
+                        .catch(error => {
+                            console.error(error);
+                        });
+                </script>
+            </div>
+        </div>
+
+</body>
+
+</html>
+{{-- end renungan --}}
